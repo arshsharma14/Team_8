3419c119e5f8d966e7eecfa3ffb4edcb3ce0910b
arshsharma14 <99560070+arshsharma14@users.noreply.github.com>
2024-10-08 15:08:03 -0700

    ph category wrangling
    made ph categories in soil and wetlands metadata, fixed some NA and naming issues

--- wrangling/Wetlands_selected.R
+++ wrangling/Wetlands_selected.R
@@ -14,4 +14,19 @@ wetlands_selected <- wetlands[, c("#SampleID", "env_feature", "env_material", "e
                                "total_organic_carb_percent","total_inorganic_carbon_percent",  
                                "total_carbon_percent", "soil_total_inorganic_nitrogen", 
                                "soil_nitrogen_units", "total_nitrogen_percent", "ph")]
+str(wetlands_selected$ph)
+wetlands_selected$ph <- as.numeric(wetlands_selected$ph)
 
+
+min(wetlands_selected$ph, na.rm = TRUE)
+max(wetlands_selected$ph, na.rm = TRUE)
+
+bins <- c(-Inf, 3, 5, 6.5, 7.5, 9, 11, Inf) # Defines breaks/boundaries for each category
+
+bin_labels <- c("Strongly Acidic", "Moderately Acidic", "Slightly Acidic", 
+                "Neutral", "Slightly Basic", "Moderately Basic", "Strongly Basic")
+
+wetlands_selected$ph_category <- cut(wetlands_selected$ph, # make new column "ph_category"
+                                     breaks = bins,        # cut function: divides numeric into intervals and labels them according to assigned labels
+                                     labels = bin_labels, 
+                                     right = FALSE)
--- wrangling/wrangle_soil.R
+++ wrangling/wrangle_soil.R
@@ -3,9 +3,30 @@ library(tidyverse)
 library(readxl)
 
 # open soil metadata
-meta <- read_xlsx("soil_metadata.xlsx")
+soil_meta <- read_xlsx("soil_metadata.xlsx")
 
 # select for needed columns
-meta_select <- select(meta, `#SampleID`, `Sample Accession`, `Sample Alias`, Ecozone, Region, Site, `Environmental Source`, `Phylogenetic Marker`, `Target Gene Subfragment`, `Collection Date`, `Sampling Depth`, Elevation, `Mean Annual Temperature (Celsius)`, `Total Carbon`, `Total Nitrogen`, pH, `CN Ratio`)
+soil_select <- select(soil_meta, `#SampleID`, `Sample Accession`, `Sample Alias`, Ecozone, Region, Site, 
+                      `Environmental Source`, `Phylogenetic Marker`, `Target Gene Subfragment`, `Collection Date`, 
+                      `Sampling Depth`, Elevation, `Mean Annual Temperature (Celsius)`, `Total Carbon`, `Total Nitrogen`, pH, `CN Ratio`)
 
-meta_select <- rename(meta_select, `elevation_m` = `Elevation`, `sampling_depth_m` = `Sampling Depth`, `mean_annual_temp_C` = `Mean Annual Temperature (Celsius)`, `ph` = `pH`, `cn_ratio` = `CN Ratio`, `total_carbon` = `Total Carbon`, `total_nitrogen` = `Total Nitrogen`)                 
+soil_select <- rename(soil_select, `elevation_m` = `Elevation`, `sampling_depth_m` = `Sampling Depth`, 
+                      `mean_annual_temp_C` = `Mean Annual Temperature (Celsius)`, `ph` = `pH`, `cn_ratio` = 
+                        `CN Ratio`, `total_carbon` = `Total Carbon`, `total_nitrogen` = `Total Nitrogen`)                 
+
+soil_select$ph <- as.numeric(soil_select$ph)
+
+
+soil_bins <- c(-Inf, 3, 5, 6.5, 7.5, 9, 11, Inf)
+
+soil_bin_labels <- c("Strongly Acidic", "Moderately Acidic", "Slightly Acidic", 
+                    "Neutral", "Slightly Basic", "Moderately Basic", "Strongly Basic")
+
+soil_select$ph[soil_select$ph == 0.0] <- NA # Some of the 0.0 values in ph column are supposed to be NA
+sum(is.na(soil_select$ph))
+sum(!is.na(soil_select$ph))
+
+soil_select$ph_category <- cut(soil_select$ph, # make new column "ph_category"
+                               breaks = soil_bins,        # cut function: divides numeric into intervals and labels them according to assigned labels
+                               labels = soil_bin_labels, 
+                               right = FALSE)
