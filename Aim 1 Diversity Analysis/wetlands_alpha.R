a1b0512f1c253f2fcd72ec94bf38d9a778222390
rkailay <ramdeepkailay@gmail.com>
2024-11-15 11:10:54 -0800

    updated aim 3 files

---
+++ Aim 1 Diversity Analysis/wetlands_alpha.R
@@ -0,0 +1,183 @@
+# Load in Libraries
+library(phyloseq)
+library(ape) # importing trees
+library(tidyverse)
+library(vegan)
+library(picante)
+
+#### Load data ####
+# Change file paths as necessary
+metafp <- "../wrangling/wetlands_metadata.tsv"
+meta <- read_delim(metafp, delim="\t")
+
+otufp <- "../qiime/Wetlands/wetlands_export/table_export/feature-table.txt"
+otu <- read_delim(file = otufp, delim="\t", skip=1)
+
+taxfp <- "../qiime/Wetlands/wetlands_export/taxonomy_export/taxonomy.tsv"
+tax <- read_delim(taxfp, delim="\t")
+
+phylotreefp <- "../qiime/Wetlands/wetlands_export/rooted_tree_export/tree.nwk"
+phylotree <- read.tree(phylotreefp)
+
+#### Format OTU table ####
+# OTU tables should be a matrix
+# with rownames and colnames as OTUs and sampleIDs, respectively
+# Note: tibbles do not allow rownames so if you imported with read_delim, change back
+
+# save everything except first column (OTU ID) into a matrix
+otu_mat <- as.matrix(otu[,-1])
+# Make first column (#OTU ID) the rownames of the new matrix
+rownames(otu_mat) <- otu$`#OTU ID`
+# Use the "otu_table" function to make an OTU table
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE) 
+class(OTU)
+
+#### Format sample metadata ####
+# Filter meta DataFrame to exclude "Very Low" and "Very High" in cn_category
+meta <- meta[!meta$cn_category %in% c("Very Low", "Very High"), ]
+# Save everything except sampleid as new data frame
+samp_df <- as.data.frame(meta[,-1])
+# Make sampleids the rownames
+rownames(samp_df)<- meta$'#SampleID'
+# Make phyloseq sample data with sample_data() function
+SAMP <- sample_data(samp_df)
+class(SAMP)
+
+#### Formatting taxonomy ####
+# Convert taxon strings to a table with separate taxa rank columns
+tax_mat <- tax %>% select(-Confidence)%>%
+  separate(col=Taxon, sep="; "
+           , into = c("Domain","Phylum","Class","Order","Family","Genus","Species")) %>%
+  as.matrix() # Saving as a matrix
+# Save everything except feature IDs 
+tax_mat <- tax_mat[,-1]
+# Make sampleids the rownames
+rownames(tax_mat) <- tax$`Feature ID`
+# Make taxa table
+TAX <- tax_table(tax_mat)
+class(TAX)
+
+#### Create phyloseq object ####
+# Merge all into a phyloseq object
+mpt <- phyloseq(OTU, SAMP, TAX, phylotree)
+
+#### Looking at phyloseq object #####
+# View components of phyloseq object with the following commands
+otu_table(mpt)
+sample_data(mpt)
+tax_table(mpt)
+phy_tree(mpt)
+
+
+#### Accessor functions ####
+# These functions allow you to see or summarise data
+
+# If we look at sample variables and decide we only want some variables, we can view them like so:
+sample_variables(mpt)
+# colnames(sample_data(atamaca))
+get_variable(mpt, c("cn_category")) # equivalent to "select" in tidyverse
+
+## Let's say we want to filter OTU table by sample. 
+# We can first view sample names:
+sample_names(mpt)
+# How many samples do we have?
+nsamples(mpt)
+# What is the sum of reads in each sample?
+sample_sums(mpt)
+# Save the sample names of the 3 samples with the most reads
+getsamps <- names(sort(sample_sums(mpt), decreasing = TRUE)[1:3])
+# filter to see taxa abundances for each sample
+get_taxa(mpt, getsamps) 
+
+## Conversely, let's say we want to compare OTU abundance of most abundant OTU across samples
+# Look at taxa names
+taxa_names(mpt)
+# How many taxa do we have? (ASVs)
+ntaxa(mpt)
+# What is the total read count for each taxa?
+taxa_sums(mpt)
+# Let's find the top 3 most abundant taxa
+gettaxa <- names(sort(taxa_sums(mpt), decreasing = TRUE)[1:3] )
+get_sample(mpt, gettaxa)
+
+
+######### ANALYZE ##########
+# Remove non-bacterial sequences, if any
+mpt_filt <- subset_taxa(mpt,  Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+# Remove ASVs that have less than 5 counts total
+mpt_filt_nolow <- filter_taxa(mpt_filt, function(x) sum(x)>5, prune = TRUE)
+# Remove samples with less than 100 reads - poor sequencing run
+mpt_filt_nolow_samps <- prune_samples(sample_sums(mpt_filt_nolow)>100, mpt_filt_nolow)
+# Remove samples where cn_category is na
+mpt_final <- subset_samples(mpt_filt_nolow_samps, !is.na(cn_category) )
+
+# Rarefy samples
+# rngseed sets a random number. If you want to reproduce this exact analysis, you need
+# to set rngseed the same number each time
+# t transposes the table to use rarecurve function
+# cex decreases font size
+rarecurve(t(as.data.frame(otu_table(mpt_final))), cex=0.1)
+mpt_rare <- rarefy_even_depth(mpt_final, rngseed = 8, sample.size = XXXX)
+
+
+#### Alpha diversity ######
+plot_richness(mpt_rare) 
+
+plot_richness(mpt_rare, measures = c("Shannon","Chao1")) 
+
+gg_richness <- plot_richness(mpt_rare, x = "subject", measures = c("Shannon","Chao1")) +
+  xlab("Subject ID") +
+  geom_boxplot()
+gg_richness
+
+ggsave(filename = "plot_richness.png"
+       , gg_richness
+       , height=4, width=6)
+
+estimate_richness(mpt_rare)
+
+# phylogenetic diversity
+
+# calculate Faith's phylogenetic diversity as PD
+phylo_dist <- pd(t(otu_table(mpt_rare)), phy_tree(mpt_rare),
+                 include.root=F) 
+?pd
+
+# add PD to metadata table
+sample_data(mpt_rare)$PD <- phylo_dist$PD
+
+# plot any metadata category against the PD
+plot.pd <- ggplot(sample_data(mpt_rare), aes(subject, PD)) + 
+  geom_boxplot() +
+  xlab("Subject ID") +
+  ylab("Phylogenetic Diversity")
+
+# view plot
+plot.pd
+
+
+#### Taxonomy bar plots ####
+
+# Plot bar plot of taxonomy
+plot_bar(mpt_rare, fill="Phylum") 
+
+# Convert to relative abundance
+mpt_RA <- transform_sample_counts(mpt_rare, function(x) x/sum(x))
+
+# To remove black bars, "glom" by phylum first where phylum is taxonomic group of interest
+mpt_phylum <- tax_glom(mpt_RA, taxrank = "Phylum", NArm=FALSE)
+
+plot_bar(mpt_phylum, fill="Phylum") + 
+  facet_wrap(.~subject, scales = "free_x")
+
+gg_taxa <- plot_bar(mpt_phylum, fill="Phylum") + 
+  facet_wrap(.~subject, scales = "free_x")
+gg_taxa
+
+ggsave("plot_taxonomy.png"
+       , gg_taxa
+       , height=8, width =12)
+
+
+
+
